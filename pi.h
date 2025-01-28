f8989dd4099a0e6f27f4af15e84c859603e21650
scwadr <69246632+scwadr@users.noreply.github.com>
2025-01-28 14:24:52 -0500

    Add files via upload
    fixed random seed?

--- pi.h
+++ pi.h
@@ -18,12 +18,12 @@ double pi_calc(long int n) {
     int rank, p;
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    srand(time(NULL) + rank);
 
     int bibis = n % p;
     long int sum = 0;
     for (int j = 0; j < ((rank < bibis) ? n / p + 1 : n / p); j++) 
     {
-        srand(time(NULL) + rank);
         long double x = (1.0 * rand()) / RAND_MAX;
         long double y = (1.0 * rand()) / RAND_MAX;
         if (x * x + y * y - 1.0 <= 0.0) {
